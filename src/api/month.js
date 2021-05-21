89dc3a91075f3ced65680a33f1abe277d08acb4f
Christine <huangc909@gmail.com>
2021-05-21 11:34:44 -0400

    Created alternative for creating months and days, but still comes out unordered

--- src/api/month.js
+++ src/api/month.js
@@ -15,33 +15,183 @@ export const month = (schoolYear, schoolYearId, monthName, user) => {
     }
   })
 }
-//
-// export const august = (schoolYear, schoolYearId, user) => {
-//   return axios({
-//     method: 'POST',
-//     url: apiUrl + `/schoolYears/${schoolYearId}/months`,
-//     headers: {
-//       'Authorization': `Bearer ${user.token}`
-//     },
-//     data: {
-//       month: {
-//         month: 'August'
-//       }
-//     }
-//   })
-// }
-//
-// export const september = (schoolYear, schoolYearId, user) => {
-//   return axios({
-//     method: 'POST',
-//     url: apiUrl + `/schoolYears/${schoolYearId}/months`,
-//     headers: {
-//       'Authorization': `Bearer ${user.token}`
-//     },
-//     data: {
-//       month: {
-//         month: 'September'
-//       }
-//     }
-//   })
-// }
+
+export const august = (schoolYear, schoolYearId, user) => {
+  return axios({
+    method: 'POST',
+    url: apiUrl + `/schoolYears/${schoolYearId}/months`,
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    },
+    data: {
+      month: {
+        month: 'August'
+      }
+    }
+  })
+}
+
+export const september = (schoolYear, schoolYearId, user) => {
+  return axios({
+    method: 'POST',
+    url: apiUrl + `/schoolYears/${schoolYearId}/months`,
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    },
+    data: {
+      month: {
+        month: 'September'
+      }
+    }
+  })
+}
+
+export const october = (schoolYear, schoolYearId, user) => {
+  return axios({
+    method: 'POST',
+    url: apiUrl + `/schoolYears/${schoolYearId}/months`,
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    },
+    data: {
+      month: {
+        month: 'October'
+      }
+    }
+  })
+}
+
+export const november = (schoolYear, schoolYearId, user) => {
+  return axios({
+    method: 'POST',
+    url: apiUrl + `/schoolYears/${schoolYearId}/months`,
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    },
+    data: {
+      month: {
+        month: 'November'
+      }
+    }
+  })
+}
+
+export const december = (schoolYear, schoolYearId, user) => {
+  return axios({
+    method: 'POST',
+    url: apiUrl + `/schoolYears/${schoolYearId}/months`,
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    },
+    data: {
+      month: {
+        month: 'December'
+      }
+    }
+  })
+}
+
+export const january = (schoolYear, schoolYearId, user) => {
+  return axios({
+    method: 'POST',
+    url: apiUrl + `/schoolYears/${schoolYearId}/months`,
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    },
+    data: {
+      month: {
+        month: 'January'
+      }
+    }
+  })
+}
+
+export const february = (schoolYear, schoolYearId, user) => {
+  return axios({
+    method: 'POST',
+    url: apiUrl + `/schoolYears/${schoolYearId}/months`,
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    },
+    data: {
+      month: {
+        month: 'February'
+      }
+    }
+  })
+}
+
+export const march = (schoolYear, schoolYearId, user) => {
+  return axios({
+    method: 'POST',
+    url: apiUrl + `/schoolYears/${schoolYearId}/months`,
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    },
+    data: {
+      month: {
+        month: 'March'
+      }
+    }
+  })
+}
+
+export const april = (schoolYear, schoolYearId, user) => {
+  return axios({
+    method: 'POST',
+    url: apiUrl + `/schoolYears/${schoolYearId}/months`,
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    },
+    data: {
+      month: {
+        month: 'April'
+      }
+    }
+  })
+}
+
+export const may = (schoolYear, schoolYearId, user) => {
+  return axios({
+    method: 'POST',
+    url: apiUrl + `/schoolYears/${schoolYearId}/months`,
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    },
+    data: {
+      month: {
+        month: 'May'
+      }
+    }
+  })
+}
+
+export const june = (schoolYear, schoolYearId, user) => {
+  return axios({
+    method: 'POST',
+    url: apiUrl + `/schoolYears/${schoolYearId}/months`,
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    },
+    data: {
+      month: {
+        month: 'June'
+      }
+    }
+  })
+}
+
+export const july = (schoolYear, schoolYearId, user) => {
+  return axios({
+    method: 'POST',
+    url: apiUrl + `/schoolYears/${schoolYearId}/months`,
+    headers: {
+      'Authorization': `Bearer ${user.token}`
+    },
+    data: {
+      month: {
+        month: 'July'
+      }
+    }
+  })
+}
--- src/components/routes/MonthsCreate.js
+++ src/components/routes/MonthsCreate.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Redirect } from 'react-router-dom'
+// import { august, september, october, november, december, january, february, march, april, may, june, july } from '../../api/month'
 import { month } from '../../api/month'
 import { day } from '../../api/day'
 
@@ -79,6 +80,205 @@ const MonthsCreate = props => {
         .catch(error => console.log(error))
     }
   }
+  //
+  // august(schoolYear, schoolYearId, user)
+  //   .then(res => {
+  //     const monthId = res.data.schoolYear.months[res.data.schoolYear.months.length - 1]._id
+  //     let num = 0
+  //     for (let i = 1; i < 32; i++) {
+  //       num = i
+  //       day(schoolYear, schoolYearId, monthId, num, user, msgAlert)
+  //     }
+  //   })
+  //   .then(() => msgAlert({
+  //     heading: 'Calendar Created',
+  //     variant: 'success'
+  //   }))
+  //   .catch(error => console.log(error))
+  //
+  // september(schoolYear, schoolYearId, user)
+  //   .then(res => {
+  //     const monthId = res.data.schoolYear.months[res.data.schoolYear.months.length - 1]._id
+  //     let num = 0
+  //     for (let i = 1; i < 31; i++) {
+  //       num = i
+  //       day(schoolYear, schoolYearId, monthId, num, user, msgAlert)
+  //     }
+  //   })
+  //   .then(() => msgAlert({
+  //     heading: 'Calendar Created',
+  //     variant: 'success'
+  //   }))
+  //   .catch(error => console.log(error))
+  //
+  // october(schoolYear, schoolYearId, user)
+  //   .then(res => {
+  //     const monthId = res.data.schoolYear.months[res.data.schoolYear.months.length - 1]._id
+  //     let num = 0
+  //     for (let i = 1; i < 32; i++) {
+  //       num = i
+  //       day(schoolYear, schoolYearId, monthId, num, user, msgAlert)
+  //     }
+  //   })
+  //   .then(() => msgAlert({
+  //     heading: 'Calendar Created',
+  //     variant: 'success'
+  //   }))
+  //   .catch(error => console.log(error))
+  //
+  // november(schoolYear, schoolYearId, user)
+  //   .then(res => {
+  //     const monthId = res.data.schoolYear.months[res.data.schoolYear.months.length - 1]._id
+  //     let num = 0
+  //     for (let i = 1; i < 31; i++) {
+  //       num = i
+  //       day(schoolYear, schoolYearId, monthId, num, user, msgAlert)
+  //     }
+  //   })
+  //   .then(() => msgAlert({
+  //     heading: 'Calendar Created',
+  //     variant: 'success'
+  //   }))
+  //   .catch(error => console.log(error))
+  //
+  // december(schoolYear, schoolYearId, user)
+  //   .then(res => {
+  //     const monthId = res.data.schoolYear.months[res.data.schoolYear.months.length - 1]._id
+  //     let num = 0
+  //     for (let i = 1; i < 32; i++) {
+  //       num = i
+  //       day(schoolYear, schoolYearId, monthId, num, user, msgAlert)
+  //     }
+  //   })
+  //   .then(() => msgAlert({
+  //     heading: 'Calendar Created',
+  //     variant: 'success'
+  //   }))
+  //   .catch(error => console.log(error))
+  //
+  // january(schoolYear, schoolYearId, user)
+  //   .then(res => {
+  //     const monthId = res.data.schoolYear.months[res.data.schoolYear.months.length - 1]._id
+  //     let num = 0
+  //     for (let i = 1; i < 32; i++) {
+  //       num = i
+  //       day(schoolYear, schoolYearId, monthId, num, user, msgAlert)
+  //     }
+  //   })
+  //   .then(() => msgAlert({
+  //     heading: 'Calendar Created',
+  //     variant: 'success'
+  //   }))
+  //   .catch(error => console.log(error))
+  //
+  // const year = parseInt(schoolYear.endYear)
+  // const leapYear = (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0)
+  // if (leapYear) {
+  //   february(schoolYear, schoolYearId, user)
+  //     .then(res => {
+  //       const monthId = res.data.schoolYear.months[res.data.schoolYear.months.length - 1]._id
+  //       let num = 0
+  //       for (let i = 1; i < 30; i++) {
+  //         num = i
+  //         day(schoolYear, schoolYearId, monthId, num, user, msgAlert)
+  //       }
+  //     })
+  //     .then(() => msgAlert({
+  //       heading: 'Calendar Created',
+  //       variant: 'success'
+  //     }))
+  //     .catch(error => console.log(error))
+  // } else {
+  //   february(schoolYear, schoolYearId, user)
+  //     .then(res => {
+  //       const monthId = res.data.schoolYear.months[res.data.schoolYear.months.length - 1]._id
+  //       let num = 0
+  //       for (let i = 1; i < 29; i++) {
+  //         num = i
+  //         day(schoolYear, schoolYearId, monthId, num, user, msgAlert)
+  //       }
+  //     })
+  //     .then(() => msgAlert({
+  //       heading: 'Calendar Created',
+  //       variant: 'success'
+  //     }))
+  //     .catch(error => console.log(error))
+  // }
+  //
+  // march(schoolYear, schoolYearId, user)
+  //   .then(res => {
+  //     const monthId = res.data.schoolYear.months[res.data.schoolYear.months.length - 1]._id
+  //     let num = 0
+  //     for (let i = 1; i < 32; i++) {
+  //       num = i
+  //       day(schoolYear, schoolYearId, monthId, num, user, msgAlert)
+  //     }
+  //   })
+  //   .then(() => msgAlert({
+  //     heading: 'Calendar Created',
+  //     variant: 'success'
+  //   }))
+  //   .catch(error => console.log(error))
+  //
+  // april(schoolYear, schoolYearId, user)
+  //   .then(res => {
+  //     const monthId = res.data.schoolYear.months[res.data.schoolYear.months.length - 1]._id
+  //     let num = 0
+  //     for (let i = 1; i < 31; i++) {
+  //       num = i
+  //       day(schoolYear, schoolYearId, monthId, num, user, msgAlert)
+  //     }
+  //   })
+  //   .then(() => msgAlert({
+  //     heading: 'Calendar Created',
+  //     variant: 'success'
+  //   }))
+  //   .catch(error => console.log(error))
+  //
+  // may(schoolYear, schoolYearId, user)
+  //   .then(res => {
+  //     const monthId = res.data.schoolYear.months[res.data.schoolYear.months.length - 1]._id
+  //     let num = 0
+  //     for (let i = 1; i < 32; i++) {
+  //       num = i
+  //       day(schoolYear, schoolYearId, monthId, num, user, msgAlert)
+  //     }
+  //   })
+  //   .then(() => msgAlert({
+  //     heading: 'Calendar Created',
+  //     variant: 'success'
+  //   }))
+  //   .catch(error => console.log(error))
+  //
+  // june(schoolYear, schoolYearId, user)
+  //   .then(res => {
+  //     const monthId = res.data.schoolYear.months[res.data.schoolYear.months.length - 1]._id
+  //     let num = 0
+  //     for (let i = 1; i < 31; i++) {
+  //       num = i
+  //       day(schoolYear, schoolYearId, monthId, num, user, msgAlert)
+  //     }
+  //   })
+  //   .then(() => msgAlert({
+  //     heading: 'Calendar Created',
+  //     variant: 'success'
+  //   }))
+  //   .catch(error => console.log(error))
+  //
+  // july(schoolYear, schoolYearId, user)
+  //   .then(res => {
+  //     const monthId = res.data.schoolYear.months[res.data.schoolYear.months.length - 1]._id
+  //     let num = 0
+  //     for (let i = 1; i < 32; i++) {
+  //       num = i
+  //       day(schoolYear, schoolYearId, monthId, num, user, msgAlert)
+  //     }
+  //   })
+  //   .then(() => msgAlert({
+  //     heading: 'Calendar Created',
+  //     variant: 'success'
+  //   }))
+  //   .catch(error => console.log(error))
 
   return (
     <Redirect to={`/schoolyears/${schoolYearId}`} />
